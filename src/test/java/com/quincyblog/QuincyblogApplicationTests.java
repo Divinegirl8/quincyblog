faa840cf7880970c837f87e9bf795e7060679623
James DivineMercy <iamoluchimercy@gmail.com>
2024-07-04 20:27:21 +0100

    first commit

---
+++ src/test/java/com/quincyblog/QuincyblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quincyblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuincyblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
