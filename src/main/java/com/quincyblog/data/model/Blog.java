ba3b7d9c1014fc28c5449d22a7803e1b9c501a3b
James DivineMercy <iamoluchimercy@gmail.com>
2024-07-04 21:00:29 +0100

    first commit

---
+++ src/main/java/com/quincyblog/data/model/Blog.java
@@ -0,0 +1,12 @@
+package com.quincyblog.data.model;
+
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@RequiredArgsConstructor
+
+public class Blog {
+}
